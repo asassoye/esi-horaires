6aa3b82e0ec730944c846097a6cf8cdd3b6bdec7
Matthieu Brandt <44422@etu.he2b.be>
2020-09-15 15:35:15 +0200

    Code cleanup + doc + updated lib + plain JavaScript + features
    Code cleanup
    Fixed codes
    Added doc
    Updated lib fullcalendar to v5.
    Removed jquery references in code
    Added features

--- js/config.js
+++ js/config.js
@@ -1,100 +1,154 @@
-
-var REP = "ical/2020-2021/q1/"
-
-function data_req (url, callback) {
-    req = new XMLHttpRequest()
-    req.addEventListener('load', callback)
-    req.open('GET', url)
-    req.send()
-}
-
-function load_ics(ics){
-    data_req(ics.url, function(){
-        $('#calendar').fullCalendar('addEventSource', fc_events(this.response, ics.event_properties))
-    })
-    // Set link in textarea
-    var full_url = "http://" + $(location).attr('hostname') + "/" + ics.url;
-    $('#ical').val(full_url);
-    $('.valid-feedback').hide();
-    // Set download link
-    $('#download').attr('href', ics.url);
-}
-
-function initialTimetable() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const query = urlParams.get('q');
-    if (query) {
-	$("#select").val(query);
-    }
-    return $("#select").val();
-}
-
-$(document).ready(function() {
-    $('#calendar').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay,listWeek'
+document.addEventListener("DOMContentLoaded", () => {
+    const calendarEl = document.getElementById("calendar");
+    const select = document.getElementById("select");
+    const ical = document.getElementById("ical");
+    const icalButton = document.getElementById("icalbutton");
+    const addToCalendarButton = document.getElementById("addToCalendar");
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: "timeGridWeek",
+        weekends: false,
+        headerToolbar: {
+            start: "prev,next today",
+            center: "title",
+            end: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
         },
-        defaultView: 'agendaWeek',
-        hiddenDays: [0,6],
-        locale: 'fr',
-        eventRender: function(event, element, view) {
-            if (event.location) element.find(".fc-title").append(" - " + event.location);
+        locale: "fr",
+        slotMinTime: "08:00:00",
+        slotMaxTime: "22:00:00",
+        buttonText: {
+            list: "Semainier",
         },
-        businessHours: {
-            // days of week. an array of zero-based day of week integers (0=Sunday)
-            dow: [ 1, 2, 3, 4, 5 ], // Monday - Thursday
-            start: '08:00', 
-            end: '18:00', 
+        fixedWeekCount: false,
+        showNonCurrentDates: false,
+        eventDidMount: ({event, el, view}) => {
+            if (!event.title.endsWith(event.extendedProps.location) && event.extendedProps.location) {
+                event.setProp("title", `${event.title} - ${event.extendedProps.location}`);
+            }
+            el.removeAttribute("href");
         },
-        minTime: '08:00:00',
-        maxTime: '22:00:00',
-        navLinks: true,
-        buttonText: {list: 'Semainier'}
+        dateClick: (info) => {
+
+        }
     });
+    calendar.render();
 
-    function load_timetable(timetable) {
-	load_ics({
-            url: REP + timetable + '.ics',
-            event_properties: {color: hash_color(timetable)}
-	});
-    }
-
-    // Load first timetable
-    load_timetable(initialTimetable());
-
-    // Action on menu
-    $('#select').on('change', function() {
-        $('#calendar').fullCalendar('removeEvents');
-	load_timetable($(this).val());
-    });
-
-    // Copy link to clipboard
-    $(function() {
-        $('#ical').click(function() {
-          $(this).focus();
-          $(this).select();
-          var success = document.execCommand('copy');
-          if (success) {
-            $('.valid-feedback').removeClass("invisible");    
-            $('.valid-feedback').show();    
-          };          
-        });
-     });
-
-     
-    $(function() {
-        $('#icalbutton').click(function(){
-            var copyText = document.getElementById("ical");
-            copyText.focus();
-            copyText.select();
-            copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-            var success = document.execCommand("copy");        
-            if (success) {
-                $('.valid-feedback').removeClass("invisible");    
-                $('.valid-feedback').show();    
-              };
+    /**
+     * Action on menu
+     */
+    select.addEventListener("change", () => {
+        load_ics(calendar, ical, {
+            url: `${select.value}.ics`,
+            event_properties: {
+                color: hash_color(select.value),
+            },
         });
     });
-})
+
+    /**
+     * Copy link to clipboard
+     */
+    ical.addEventListener("click", () => {
+        copyCalLink(ical);
+    });
+
+    /**
+     * Copy link to clipboard
+     */
+    icalButton.addEventListener("click", () => {
+        copyCalLink(ical);
+    });
+
+    /**
+     * Load first timetable
+     */
+    initialTimetable(select);
+
+    /**
+     * Initializes the timetable
+     * 
+     * @param {HTMLSelectElement} select the select element containing all courses
+     * @returns {void}
+     */
+    function initialTimetable(select) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const query = urlParams.get("q");
+        if (query) {
+            select.value = query;
+        }
+        select.dispatchEvent(new Event("change"));
+    }
+
+    /**
+     * Loads an ics on the calendar
+     * 
+     * @param {Fullcalendar.Calendar} calendar the calendar to load ics to
+     * @param {HTMLInputElement} ical the link to the ics
+     * @param {{url: string, event_properties: {color: string}}} ics the ics to add to the calendar
+     * @returns {void}
+     */
+    function load_ics(calendar, ical, ics) {
+        calendar.removeAllEvents();
+        const fullUrl = `http://${location.host}/ical/2020-2021/q1/${ics.url}`;
+        fetch(fullUrl)
+        .then(response => {
+            if(response.status < 200 || response.status > 300) {
+                throw new Error("Not found");
+            }
+            return response;
+        })
+        .then(response => response.text())
+        .then((response) => {
+            calendar.batchRendering(() => {
+                calendar.addEventSource(fc_events(response, ics.event_properties));
+            });
+            Array.from(document.getElementsByClassName("valid-feedback")).forEach(e => {
+                e.style.display = "none";
+            });
+            ical.value = fullUrl;
+            addToCalendarButton.href = `https://www.google.com/calendar/render?cid=${fullUrl}`;
+        })
+        .then(() => calendar.render())
+        .catch(e => console.error(e.message));
+    }
+
+    /**
+     * Copies the ics link to the user's clipboard
+     * 
+     * @param {HTMLInputElement} ical the link to the ics
+     * @returns {void}
+     */
+    function copyCalLink(ical) {
+        ical.focus();
+        ical.select();
+        ical.setSelectionRange(0, 99999); /*For mobile devices*/
+        if (document.execCommand("copy")) {
+            Array.from(document.getElementsByClassName("valid-feedback")).forEach(e => {
+                e.style.display = "block";
+            });
+        }
+    }
+
+    /**
+     * Returns a number based on a string
+     * 
+     * @param {string} string the string to generate the number from
+     * @returns {number} a number based on the provided string
+     */
+    function hash_code(string) {
+        let h = 0;
+        for (let i = 0; i < string.length; ++i) {
+            h = string.charCodeAt(i) + ((h << 5) - h);
+        }
+        return h;
+    }
+
+    /**
+     * Returns a hsl color based on a string
+     * 
+     * @param {string} string the string to generate the color from
+     * @returns {string} a hsl color based on the provided string
+     */
+    function hash_color(string) {
+        return `hsl(${hash_code(string) % 360}, 30%, 50%)`;
+    }
+});
--- js/ical_fullcalendar.js
+++ js/ical_fullcalendar.js
@@ -1,79 +1,99 @@
-// Depends on ./ical_events.js
+/**
+ * Depends on ./ical_events.js
+ */ 
+const recur_events = [];
 
-recur_events = []
-
+/**
+ * Removes non alphanumeric characters
+ * 
+ * @param {string} string 
+ * @returns {string}
+ */
 function an_filter(string) {
-    // remove non alphanumeric chars
-    return string.replace(/[^\w\s]/gi, '')
+    return string.replace(/[^\w\s]/gi, "");
 }
 
 function moment_icaltime(moment, timezone) {
     // TODO timezone
-    return new ICAL.Time().fromJSDate(moment.toDate())
+    return new ICAL.Time().fromJSDate(moment.toDate());
 }
 
 function expand_recur_events(start, end, timezone, events_callback) {
-    events = []
-    recur_events.forEach(function(event, i){
-      event_properties = event.event_properties
-      expand_recur_event(event, moment_icaltime(start, timezone), moment_icaltime(end, timezone), function(event){
-        fc_event(event, function(event){
-          events.push(merge_events(event_properties, merge_events({className:['recur-event']}, event)))
-        })
-      })
-    })
-    events_callback(events)
+    const events = [];
+    recur_events.forEach((event) => {
+        event_properties = event.event_properties;
+        expand_recur_event(event, moment_icaltime(start, timezone), moment_icaltime(end, timezone), (event) => {
+            fc_event(event, (event) => {
+                events.push(merge_events(event_properties, merge_events({className: ["recur-event"]}, event)));
+            });
+        });
+    });
+    events_callback(events);
 }
 
 function fc_events(ics, event_properties) {
-    events = []
+    const events = [];
     ical_events(
         ics,
-        function(event){
-            fc_event(event, function(event){
-                events.push(merge_events(event_properties, event))
-            })
+        (event) => {
+            fc_event(event, (event) => {
+                events.push(merge_events(event_properties, event));
+            });
         },
-        function(event){
-            event.event_properties = event_properties
-            recur_events.push(event)
+        (event) => {
+            event.event_properties = event_properties;
+            recur_events.push(event);
         }
-    )
-    return events
+    );
+    return events;
 }
 
+/**
+ * Merges the first event into the second one
+ * 
+ * @param {Fullcalendar.Event} e an event
+ * @param {Fullcalendar.Event} f another event
+ * @returns {Fullcalendar.Event} the merged event
+ */
 function merge_events(e, f) {
     // f has priority
-    for (k in e) {
-        if (k == 'className') {
-            f[k] = [].concat(f[k]).concat(e[k])
-        } else if (! f[k]) {
-            f[k] = e[k]
+    for (let k in e) {
+        if (k == "className") {
+            f[k] = [].concat(f[k]).concat(e[k]);
+        } else if (!f[k]) {
+            f[k] = e[k];
         }
     }
-    return f
+    return f;
 }
 
+/**
+ * 
+ * 
+ * @param {Fullcalendar.Event} event 
+ * @param {(e: Fullcalendar.Event) => void} event_callback 
+ * @returns {void}
+ */
 function fc_event(event, event_callback) {
-    e = {
-        title:event.getFirstPropertyValue('summary'),
-        url:event.getFirstPropertyValue('url'),
-        id:event.getFirstPropertyValue('uid'),
-        className:['event-'+an_filter(event.getFirstPropertyValue('uid'))],
-        allDay:false,
-        location:event.getFirstPropertyValue('location'),
-        description:event.getFirstPropertyValue('description')
+    const e = {
+        title: event.getFirstPropertyValue("summary"),
+        url: event.getFirstPropertyValue("url"),
+        id: event.getFirstPropertyValue("uid"),
+        className: ["event-" + an_filter(event.getFirstPropertyValue("uid"))],
+        allDay: false,
+        location: event.getFirstPropertyValue("location"),
+        description: event.getFirstPropertyValue("description"),
+    };
+    try {
+        e.start = event.getFirstPropertyValue("dtstart").toJSDate();
+    } catch (TypeError) {
+        console.debug(`Undefined "dtstart", vevent skipped.`);
+        return;
     }
     try {
-        e['start'] = event.getFirstPropertyValue('dtstart').toJSDate()
+        e.end = event.getFirstPropertyValue("dtend").toJSDate();
     } catch (TypeError) {
-        console.debug('Undefined "dtstart", vevent skipped.')
-        return
+        e.allDay = true;
     }
-    try {
-        e['end'] = event.getFirstPropertyValue('dtend').toJSDate()
-    } catch (TypeError) {
-        e['allDay'] = true
-    }
-    event_callback(e)
+    event_callback(e);
 }
